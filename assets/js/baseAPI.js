58c62ac555ac0821a775a14e38d9bf49d856c0d1
xujie <2275817182@qq.com>
2021-07-30 09:53:52 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,23 @@ $.ajaxPrefilter(function(options) {
 
     //在发起真正的Ajax请求之前，统一拼接请求的跟路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+    //统一为有权限地接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+
+    //全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // console.log('执行了 complete的函数回调');
+        // console.log(res);
+        //在complete回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.强制清空 token
+            localStorage.removeItem('token')
+                //2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
